0942702d1de481ed62ce8bc82fb1a0488af0bd6c
mmjck <msantosjck@gmail.com>
2024-05-16 17:00:56 -0400

    init code blog

---
+++ codeblog/src/test/java/com/example/mmjck/codeblog/CodeblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mmjck.codeblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
