761657b0398b58f8d8249a3e8b460dca7d33a1dd
mmjck <msantosjck@gmail.com>
2024-05-17 19:09:42 -0400

    moved to new branch

--- codeblog/src/main/java/com/example/mmjck/codeblog/repository/CodeblogRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.mmjck.codeblog.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.mmjck.codeblog.model.Post;
-
-public interface CodeblogRepository extends JpaRepository<Post, Long>{}
