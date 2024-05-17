cf1cd6484a417c1f2c40eaa3fe81da2a793b57ea
mmjck <msantosjck@gmail.com>
2024-05-17 19:01:23 -0400

    feat: added producer

--- micro-service/user/src/main/java/br/com/ms/user/service/UserService.java
+++ micro-service/user/src/main/java/br/com/ms/user/service/UserService.java
@@ -3,6 +3,7 @@ package br.com.ms.user.service;
 import org.springframework.stereotype.Service;
 
 import br.com.ms.user.model.UserModel;
+import br.com.ms.user.producers.UserProducer;
 import br.com.ms.user.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 
@@ -10,13 +11,18 @@ import jakarta.transaction.Transactional;
 public class UserService {
     final UserRepository repository;
 
-    public UserService(UserRepository repository) {
+    final UserProducer userProducer;
+
+    public UserService(UserRepository repository, UserProducer userProducer) {
         this.repository = repository;
+        this.userProducer = userProducer;
     }
 
     @Transactional
-    public UserModel save(UserModel user){
-        return this.repository.save(user);
+    public UserModel save(UserModel data){
+        var user = this.repository.save(data);
+        userProducer.publish(user);
+        return user;
     }
 
 }
