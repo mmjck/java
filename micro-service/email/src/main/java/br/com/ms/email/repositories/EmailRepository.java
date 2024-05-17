bab251f6e276bbdde73826678e00236be8e243ac
mmjck <msantosjck@gmail.com>
2024-05-17 19:01:50 -0400

    feat: added email repository

---
+++ micro-service/email/src/main/java/br/com/ms/email/repositories/EmailRepository.java
@@ -0,0 +1,9 @@
+package br.com.ms.email.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.ms.email.model.EmailModel;
+
+public interface EmailRepository extends JpaRepository<EmailModel, UUID>{}
