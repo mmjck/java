c23d12bf24a814c691bd1958639b59ae4315a645
mmjck <msantosjck@gmail.com>
2024-05-17 17:43:51 -0400

    feat: user model

---
+++ micro-service/user/src/main/java/br/com/ms/user/model/UserModel.java
@@ -0,0 +1,36 @@
+package br.com.ms.user.model;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserModel implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    
+    private String name;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
