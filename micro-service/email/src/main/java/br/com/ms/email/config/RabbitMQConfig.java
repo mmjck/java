bf5823b1f4efc958494f8fe663c87499ea147cb6
mmjck <msantosjck@gmail.com>
2024-05-17 17:34:59 -0400

    feat: added rabbitmq config

---
+++ micro-service/email/src/main/java/br/com/ms/email/config/RabbitMQConfig.java
@@ -0,0 +1,18 @@
+package br.com.ms.email.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class RabbitMQConfig {
+    @Value("${broker.queue.email.name}")
+    private String queue;
+
+    @Bean
+    Queue queue() {
+        return new Queue(queue, true);
+    }
+}
