aed4341af6e3b69f7f4b3f1b54d0874d5789dd2a
mmjck <msantosjck@gmail.com>
2024-05-17 19:01:11 -0400

    feat: added getId

--- micro-service/user/src/main/java/br/com/ms/user/model/UserModel.java
+++ micro-service/user/src/main/java/br/com/ms/user/model/UserModel.java
@@ -11,25 +11,33 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
-public class UserModel implements Serializable{
+public class UserModel implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
-    
+
     private String name;
     private String email;
+
+    public UUID getId(){
+        return id;
+    }
+
 
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
