fd8e11ecfc099df77137ff0e6feceebbc752067f
mmjck <msantosjck@gmail.com>
2024-05-18 14:18:59 -0400

    feat: create project

---
+++ messaging/src/test/java/com/mmjck/messaging/MessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmjck.messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
