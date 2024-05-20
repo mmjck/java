cf21eadc4801164d71b846c6b396349c70b0ef40
mmjck <msantosjck@gmail.com>
2024-05-20 12:35:47 -0400

    feat: tests

---
+++ email-service/src/test/java/com/mmjck/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmjck.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
