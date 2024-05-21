131dcfebfb00bb8c70d5515cac9e30e1b8ce54fa
mmjck <msantosjck@gmail.com>
2024-05-21 14:33:33 -0400

    feat: added transactions controller

---
+++ cryptography/src/main/java/com/mmjck/cryptography/controllers/TransactionsController.java
@@ -0,0 +1,71 @@
+package com.mmjck.cryptography.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mmjck.cryptography.controllers.dto.CreateTransactionRequest;
+import com.mmjck.cryptography.controllers.dto.TransactionResponse;
+import com.mmjck.cryptography.controllers.dto.UpdateTransactionRequest;
+import com.mmjck.cryptography.service.TransactionsService;
+
+@RestController
+@RequestMapping(value = "/transactions")
+public class TransactionsController {
+
+    @Autowired
+    private TransactionsService service;
+
+    @PostMapping("/")
+    public ResponseEntity<Void> create(@RequestBody CreateTransactionRequest request) {
+        this.service.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Page<TransactionResponse>> listAll(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "page_size", defaultValue = "10") int pageSize) {
+        var response = this.service.listAll(page, pageSize);
+        return ResponseEntity
+                .ok()
+                .body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TransactionResponse> findById(@PathVariable(value = "id") Long id) {
+        var response = this.service.getById(id);
+        return ResponseEntity
+                .ok()
+                .body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TransactionResponse> findById(
+                    @PathVariable(value = "id") Long id,
+                    @RequestBody UpdateTransactionRequest request
+                    ) {
+        this.service.update(id, request);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(
+                    @PathVariable(value = "id") Long id,
+                    @RequestBody UpdateTransactionRequest request
+                    ) {
+        this.service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+    
+
+}
