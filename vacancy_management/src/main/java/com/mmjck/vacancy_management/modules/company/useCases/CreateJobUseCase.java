1649d904f3d4e91dd4b91f6cf72987be5b36ae46
mmjck <msantosjck@gmail.com>
2024-05-22 17:22:52 -0400

    feat: added find of company before save

--- vacancy_management/src/main/java/com/mmjck/vacancy_management/modules/company/useCases/CreateJobUseCase.java
+++ vacancy_management/src/main/java/com/mmjck/vacancy_management/modules/company/useCases/CreateJobUseCase.java
@@ -2,7 +2,10 @@ package com.mmjck.vacancy_management.modules.company.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.mmjck.vacancy_management.exceptions.CompanyNotFoundException;
 import com.mmjck.vacancy_management.modules.company.entities.JobEntity;
+import com.mmjck.vacancy_management.modules.company.repositories.CompanyRepository;
 import com.mmjck.vacancy_management.modules.company.repositories.JobRepository;
 
 @Service
@@ -11,7 +14,14 @@ public class CreateJobUseCase {
     @Autowired
     private JobRepository repository;
     
+    @Autowired
+    CompanyRepository companyRepository;
+
+
     public JobEntity execute(JobEntity entity){
+        companyRepository.findById(entity.getCompanyId()).orElseThrow(() -> {
+            throw new CompanyNotFoundException();
+        });
         return this.repository.save(entity);
     }
 }
