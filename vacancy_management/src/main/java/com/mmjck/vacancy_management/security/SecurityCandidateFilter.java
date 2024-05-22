0fee50b46125f0844f025604d4bcc570f13196c5
mmjck <msantosjck@gmail.com>
2024-05-21 21:59:36 -0400

    feat: security candidate filter

--- vacancy_management/src/main/java/com/mmjck/vacancy_management/security/SecurityCandidateFilter.java
+++ vacancy_management/src/main/java/com/mmjck/vacancy_management/security/SecurityCandidateFilter.java
@@ -1,7 +1,6 @@
 package com.mmjck.vacancy_management.security;
 
 import java.io.IOException;
-import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
