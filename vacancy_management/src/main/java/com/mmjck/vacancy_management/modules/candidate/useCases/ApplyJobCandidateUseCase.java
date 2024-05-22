d59a5e01683c71b031e51996010ede9c54724257
mmjck <msantosjck@gmail.com>
2024-05-22 09:31:42 -0400

    feat: created apply job usecase

---
+++ vacancy_management/src/main/java/com/mmjck/vacancy_management/modules/candidate/useCases/ApplyJobCandidateUseCase.java
@@ -0,0 +1,36 @@
+package com.mmjck.vacancy_management.modules.candidate.useCases;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mmjck.vacancy_management.exceptions.JobNotFoundException;
+import com.mmjck.vacancy_management.exceptions.UserNotFoundException;
+import com.mmjck.vacancy_management.modules.candidate.repositories.CandidateRepository;
+import com.mmjck.vacancy_management.modules.company.repositories.JobRepository;
+
+@Service
+public class ApplyJobCandidateUseCase {
+    
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @Autowired
+    private JobRepository jobRepository;
+
+
+    public void execute(UUID candidateId, UUID jobID){
+
+        this.candidateRepository.findById(candidateId)
+            .orElseThrow(() -> {
+                throw new UserNotFoundException();
+        });
+
+        this.jobRepository.findById(jobID)
+            .orElseThrow(() -> {
+                throw new JobNotFoundException();
+            });
+
+    }   
+}
