63e6d6501bab7317a860ac222f3aac20a921169c
mmjck <msantosjck@gmail.com>
2024-05-21 21:56:01 -0400

    main

---
+++ vacancy_management/src/main/java/com/mmjck/vacancy_management/VacancyManagementApplication.java
@@ -0,0 +1,12 @@
+package com.mmjck.vacancy_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacancyManagementApplication.class, args);
+	}
+}
