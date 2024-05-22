b753485d33ffad021f229c3ec6351a1bae6f87f7
mmjck <msantosjck@gmail.com>
2024-05-22 14:26:14 -0400

    feat: added test for case when it is success

--- vacancy_management/src/test/java/com/mmjck/vacancy_management/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
+++ vacancy_management/src/test/java/com/mmjck/vacancy_management/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
@@ -1,6 +1,7 @@
 package com.mmjck.vacancy_management.modules.candidate.useCases;
 
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
@@ -15,8 +16,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.mmjck.vacancy_management.exceptions.JobNotFoundException;
 import com.mmjck.vacancy_management.exceptions.UserNotFoundException;
+import com.mmjck.vacancy_management.modules.candidate.entities.ApplyJobEntity;
 import com.mmjck.vacancy_management.modules.candidate.entities.CandidateEntity;
+import com.mmjck.vacancy_management.modules.candidate.repositories.ApplyJobRepository;
 import com.mmjck.vacancy_management.modules.candidate.repositories.CandidateRepository;
+import com.mmjck.vacancy_management.modules.company.entities.JobEntity;
 import com.mmjck.vacancy_management.modules.company.repositories.JobRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -29,6 +33,9 @@ public class ApplyJobCandidateUseCaseTest {
 
     @Mock
     private JobRepository jobRepository;
+
+    @Mock
+    private ApplyJobRepository applyJobRepository;
 
     @Test
     @DisplayName("Should not be able to apply job with candidate not found")
@@ -45,7 +52,7 @@ public class ApplyJobCandidateUseCaseTest {
     @DisplayName("Should not be able to apply job with not not found")
     public void applyJobWithJobNotFound() {
         UUID idCandidate = UUID.randomUUID();
-        
+
         CandidateEntity candidate = new CandidateEntity();
         candidate.setId(idCandidate);
 
@@ -59,5 +66,45 @@ public class ApplyJobCandidateUseCaseTest {
             // assertEquals(e, UserNotFoundException.class);
             assertInstanceOf(JobNotFoundException.class, e);
         }
+    }
+
+    @Test
+    @DisplayName("Should be able to create a new apply job")
+    public void applyJobSuccess() {
+        UUID idCandidate = UUID.randomUUID();
+        UUID idJob = UUID.randomUUID();
+
+        CandidateEntity candidate = new CandidateEntity();
+        candidate.setId(idCandidate);
+
+        JobEntity job = new JobEntity();
+        job.setId(idJob);
+
+        var applyJob = ApplyJobEntity.builder()
+            .candidateId(idCandidate)
+            .jobId(idJob)
+            .build();
+
+
+        var applyJobCreated = ApplyJobEntity.builder()
+                .id(UUID.randomUUID())
+                .build();
+
+        when(candidateRepository
+                .findById(idCandidate))
+                .thenReturn(Optional.of(candidate));
+
+        when(jobRepository
+                .findById(idJob))
+                .thenReturn(Optional.of(job));
+
+        when(applyJobRepository.save(applyJob))
+                .thenReturn(applyJobCreated);
+
+        var result = applyJobCandidateUseCase.execute(idCandidate, idJob);
+
+        assertInstanceOf(ApplyJobEntity.class, result);
+        assertNotNull(result.getId());
+
     }
 }
