48148d32ea9f1d60e13f76726362d8b59dbb2378
mmjck <msantosjck@gmail.com>
2024-05-29 11:56:37 -0400

    feat: added service communication

--- services/customer/src/main/java/com/customer/CustomerService.java
+++ services/customer/src/main/java/com/customer/CustomerService.java
@@ -1,23 +1,42 @@
 package com.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import lombok.AllArgsConstructor;
 
 @Service
-@AllArgsConstructor
 public class CustomerService{
-    private final CustomerRepository repository;
-    public void create(CustomerRegistrationRequest data){
+    
+    @Autowired
+    private  CustomerRepository repository;
+    
+    @Autowired
+    private  RestTemplate restTemplate;
+
+
+    public void create(CustomerDto data){
         Customer customer = Customer.builder()
-            .firstName(data.firstName())
-            .lastName(data.lastName())
-            .email(data.email())
+            .firstName(data.getFirstName())
+            .lastName(data.getLastName())
+            .email(data.getEmail())
             .build();
 
         // todo: check if email valid
         // todo: check if email not taken
-        // todo: check customer in db
-        repository.save(customer);
+        repository.saveAndFlush(customer);
+           
+        // todo: check if fraudster
+        FraudCheckResponse response = restTemplate.getForObject(
+            "http://localhost:8081/api/v1/fraud-check/{customerId}", 
+            FraudCheckResponse.class,
+            customer.getId()
+        );
+
+        if(response.isFraudster()){
+            throw new IllegalStateException("fraudster");
+        }
+
+        // todo: send notification
     }
 }
