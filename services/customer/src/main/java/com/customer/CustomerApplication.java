7801df10e38f91aa589dbf874cb12a9ba1d921fb
mmjck <msantosjck@gmail.com>
2024-05-29 20:17:28 -0400

    feat: added eureka client

--- services/customer/src/main/java/com/customer/CustomerApplication.java
+++ services/customer/src/main/java/com/customer/CustomerApplication.java
@@ -1,9 +1,11 @@
 package com.customer;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
