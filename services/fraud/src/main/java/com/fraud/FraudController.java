b8d07ea61ff82ff14f7104303ad38818c12d960b
mmjck <msantosjck@gmail.com>
2024-05-29 20:17:17 -0400

    refactor: added autowired

--- services/fraud/src/main/java/com/fraud/FraudController.java
+++ services/fraud/src/main/java/com/fraud/FraudController.java
@@ -3,20 +3,20 @@ package com.fraud;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
-@AllArgsConstructor
 @Slf4j
 public class FraudController {
     
-    private final FraudCheckHistoryService service;
+    @Autowired
+    private FraudCheckHistoryService service;
 
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse create(@PathVariable("customerId") Integer customerId) {
