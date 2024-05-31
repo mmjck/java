c6a56a8b9ee85e413f9c78011b9ab244e46e3bef
mmjck <msantosjck@gmail.com>
2024-05-31 19:05:01 -0400

    feat: added notification service

---
+++ services/notification/src/main/java/com/notification/Notification.java
@@ -0,0 +1,12 @@
+package com.notification;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class Notification {
+    public static void main(String[] args) {
+        SpringApplication.run(Notification.class, args);
+    }
+    
+}
