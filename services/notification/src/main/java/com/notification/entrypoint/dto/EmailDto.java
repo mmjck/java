1894b4c28e2b524bf1505910dadee0cc5ce778d3
mmjck <msantosjck@gmail.com>
2024-06-09 20:39:57 -0400

    email dto

---
+++ services/notification/src/main/java/com/notification/entrypoint/dto/EmailDto.java
@@ -0,0 +1,16 @@
+package com.notification.entrypoint.dto;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailDto implements Serializable {
+    private String to;
+}
