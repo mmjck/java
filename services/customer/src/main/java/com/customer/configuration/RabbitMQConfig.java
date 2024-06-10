b70549e3b38588f05e82d23f22e1d0580dee788e
mmjck <msantosjck@gmail.com>
2024-06-09 20:37:24 -0400

    rabbitmq config

---
+++ services/customer/src/main/java/com/customer/configuration/RabbitMQConfig.java
@@ -0,0 +1,17 @@
+package com.customer.configuration;
+
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+public class RabbitMQConfig {
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter(){
+        ObjectMapper mapper = new ObjectMapper();
+        return new Jackson2JsonMessageConverter(mapper);
+    }
+}
+
