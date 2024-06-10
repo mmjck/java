30d53a7412aee830228fca25f71d809cb50c36ee
mmjck <msantosjck@gmail.com>
2024-06-09 20:37:49 -0400

    email dto

---
+++ services/customer/src/main/java/com/customer/core/usecase/dto/EmailDto.java
@@ -0,0 +1,17 @@
+package com.customer.core.usecase.dto;
+
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailDto implements Serializable {
+    private String to;
+}
