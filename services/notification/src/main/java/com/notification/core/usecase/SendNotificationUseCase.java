30feb59169a23cb7e53022f003ad3f459bc66cc7
mmjck <msantosjck@gmail.com>
2024-06-09 20:38:33 -0400

    builder notification

--- services/notification/src/main/java/com/notification/core/usecase/SendNotificationUseCase.java
+++ services/notification/src/main/java/com/notification/core/usecase/SendNotificationUseCase.java
@@ -5,26 +5,27 @@ import org.springframework.stereotype.Service;
 
 import com.notification.core.entity.Notification;
 import com.notification.dataprovider.NotificationDataProvider;
-import com.notification.entrypoint.dto.NotificationDto;
+import com.notification.entrypoint.dto.EmailDto;
+// import com.notification.entrypoint.dto.NotificationDto;
 
 
 @Service
 public class SendNotificationUseCase {
     
     @Autowired
-    NotificationDataProvider dataProvider;
+    private NotificationDataProvider dataProvider;
 
-    public void execute(NotificationDto dto) {
+    public void execute(EmailDto dto) {
         try {
             Notification notification = Notification.builder()
                     .customerId(1)
-                    .subject(dto.getSubject())
-                    .to(dto.getTo())
-                    .text(dto.getText())
-                    .from(dto.getFrom())
+                    // .subject(dto.getSubject())
+                    // .to(dto.getTo())
+                    // .text(dto.getText())
+                    // .from(dto.getFrom())
                     .build();
 
-            dataProvider.sendEmail(notification);
+            this.dataProvider.sendEmail(notification);
         } catch (Exception e) {
 
         }
