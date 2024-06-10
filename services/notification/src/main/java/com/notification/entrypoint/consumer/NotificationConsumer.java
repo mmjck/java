b4caa180d3b1148c6aa9caf27700f912f618d3c2
mmjck <msantosjck@gmail.com>
2024-06-09 20:39:52 -0400

    wip

--- services/notification/src/main/java/com/notification/entrypoint/consumer/NotificationConsumer.java
+++ services/notification/src/main/java/com/notification/entrypoint/consumer/NotificationConsumer.java
@@ -1,24 +1,31 @@
 package com.notification.entrypoint.consumer;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import com.notification.core.usecase.SendNotificationUseCase;
-import com.notification.entrypoint.dto.NotificationDto;
+import com.notification.entrypoint.dto.EmailDto;
+
 
 @Component
 public class NotificationConsumer {
     @Autowired
-    SendNotificationUseCase useCase;
+    private SendNotificationUseCase useCase;
 
     @RabbitListener(queues = "${spring.rabbitmq.queue}")
-    public void receive(@Payload NotificationDto dto){
+    public void receive(
+        @Payload EmailDto dto
+        ){
         try {
-            useCase.execute(dto);
+            
+            System.out.println(" called");
+            // System.out.println(dto.getTo());
+            // this.useCase.execute(dto);
         } catch (Exception e) {
-            e.printStackTrace();            
+            // e.printStackTrace();            
         }
     }    
 }
