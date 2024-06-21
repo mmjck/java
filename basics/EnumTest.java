c48b4a7f1f16817e61dbe74dcec81086a46679be
mmjck <msantosjck@gmail.com>
2024-06-20 22:32:11 -0400

    enum and switch

---
+++ basics/EnumTest.java
@@ -0,0 +1,32 @@
+public class EnumTest {
+    Day day;
+
+    public EnumTest(Day day) {
+        this.day = day;
+    }
+
+    public void tellItLikeItIs() {
+        switch (day) {
+            case MONDAY:
+                System.out.println("AAAAHHH");
+                break;
+            case FRIDAY:
+                System.out.println("WOW");
+                break;
+            case SATURDAY:
+            case SUNDAY:
+                System.out.println("YESSSSSS.");
+                break;
+            default:
+                System.out.println("SO SO :|");
+                break;
+        }
+    }
+    public static void main(String[] args) {
+        EnumTest firstDay = new EnumTest(Day.MONDAY);
+        firstDay.tellItLikeItIs();
+
+        EnumTest thirdDay = new EnumTest(Day.WEDNESDAY);
+        thirdDay.tellItLikeItIs(); 
+    }
+}
