ac0b01ee2bea4934898a3a1283612bf5cffa67ca
mmjck <msantosjck@gmail.com>
2024-06-22 16:13:41 -0400

    removed

--- shortener-url/src/main/java/com/mmjck/shortener_url/controllers/repositories/UrlRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mmjck.shortener_url.controllers.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.mmjck.shortener_url.controllers.entities.UrlEntity;
-
-
-public interface UrlRepository extends MongoRepository<UrlEntity, String>{}
--- shortener-url/src/main/java/com/mmjck/shortener_url/useCases/createurl/dto/ShortenUrlRequest.java
+++
@@ -1,5 +0,0 @@
-package com.mmjck.shortener_url.useCases.createurl.dto;
-
-public record ShortenUrlRequest(
-    String url
-) {}
