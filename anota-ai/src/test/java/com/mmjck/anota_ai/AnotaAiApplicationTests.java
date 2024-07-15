3f0488ab0d2cf0c0cc79863617f19927ea824dc4
mmjck <msantosjck@gmail.com>
2024-07-15 18:54:19 -0400

    project

---
+++ anota-ai/src/test/java/com/mmjck/anota_ai/AnotaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmjck.anota_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
