3568fd02df7e0a5adbcabdad9f8051e155a1928e
mmjck <msantosjck@gmail.com>
2024-07-20 20:50:34 -0400

    user entity

---
+++ auth-service/src/main/java/com/mmjck/auth_service/domain/user/User.java
@@ -0,0 +1,27 @@
+package com.mmjck.auth_service.domain.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "USERS")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    private String name;
+    private String password;
+    private String email;
+}
