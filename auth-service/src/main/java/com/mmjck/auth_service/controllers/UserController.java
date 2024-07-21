1f7b3583b079eafd823f042d7b71a8efdb190378
mmjck <msantosjck@gmail.com>
2024-07-20 20:50:27 -0400

    user controller

---
+++ auth-service/src/main/java/com/mmjck/auth_service/controllers/UserController.java
@@ -0,0 +1,10 @@
+package com.mmjck.auth_service.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/user")
+public class UserController {
+    
+}
