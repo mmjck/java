bd94242c5a84d808708089a22c36d4f0f0b24db7
mmjck <msantosjck@gmail.com>
2024-07-22 21:57:39 -0400

    feat: added get profile response dto

---
+++ app/src/main/java/org/z/controllers/profile/dtos/GetProfileResponseDto.java
@@ -0,0 +1,12 @@
+package org.z.controllers.profile.dtos;
+
+import java.util.List;
+public record GetProfileResponseDto(
+    String id,
+    String name,
+    String email,
+    String login,
+    List<FollowedDto> followedDto
+) {
+    
+}
