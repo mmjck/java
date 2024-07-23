05982bb9f720d70aede0cf13ac6e12a7b7f71646
mmjck <msantosjck@gmail.com>
2024-07-22 21:57:16 -0400

    feat: added follow profile response dto

---
+++ app/src/main/java/org/z/controllers/profile/dtos/FollowProfileResponseDto.java
@@ -0,0 +1,13 @@
+package org.z.controllers.profile.dtos;
+
+import java.util.List;
+
+public record FollowProfileResponseDto(
+    String id,
+    String name,
+    String email,
+    String login,
+    List<FollowedDto> followed
+) {
+    
+}
