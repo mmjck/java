d850460f741e3fbde7b128c528f7a93ee3169735
mmjck <msantosjck@gmail.com>
2024-07-22 21:59:57 -0400

    feat: added  tweet dto

---
+++ app/src/main/java/org/z/controllers/tweet/dtos/TweetDto.java
@@ -0,0 +1,16 @@
+package org.z.controllers.tweet.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.Instant;
+
+
+public record TweetDto(
+        String id,
+        String content,
+        @JsonProperty("author_id") String authorId,
+        @JsonProperty("author_login") String authorLogin,
+        int likes,
+        int views,
+        @JsonProperty("created_at") Instant createdAt) {
+
+}
