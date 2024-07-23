87725644a6cd20ab8bdb0081f7320741ea3fe081
mmjck <msantosjck@gmail.com>
2024-07-23 16:36:17 -0400

    fix: auth controller

--- auth-service/src/main/java/com/mmjck/auth_service/controllers/AuthController.java
+++ auth-service/src/main/java/com/mmjck/auth_service/controllers/AuthController.java
@@ -1,15 +1,13 @@
 package com.mmjck.auth_service.controllers;
 
-import java.util.Optional;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mmjck.auth_service.domain.user.User;
-import com.mmjck.auth_service.domain.user.exceptions.UserNotFoundException;
+import com.mmjck.auth_service.domain.user.entity.User;
+
 import com.mmjck.auth_service.dto.LoginRequestDTO;
 import com.mmjck.auth_service.dto.RegisterRequestDTO;
 import com.mmjck.auth_service.dto.ResponseDTO;
@@ -18,13 +16,11 @@ import com.mmjck.auth_service.services.UserService;
 
 import org.springframework.web.bind.annotation.PostMapping;
 
-
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-    private UserService userService;
+    private final UserService userService;
     private final PasswordEncoder passwordEncoder;
-
     private final TokenService tokenService;
 
     public AuthController(UserService userService, PasswordEncoder passwordEncoder, TokenService tokenService) {
@@ -34,31 +30,21 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequestDTO requestDto){
-        User user = this.userService.findByEmail(requestDto.email()).orElseThrow(() -> new UserNotFoundException());
+    public ResponseEntity<?> login(@RequestBody LoginRequestDTO requestDto) {
+        User user = this.userService.findByEmail(requestDto.email());
 
-        
-        if(passwordEncoder.matches(requestDto.password(), user.getPassword())){
+        if (passwordEncoder.matches(requestDto.password(), user.getPassword())) {
             String token = this.tokenService.generateToken(user);
             return ResponseEntity.ok().body(new ResponseDTO(user.getName(), token));
         }
-
 
         return ResponseEntity.badRequest().build();
 
     }
 
-
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody RegisterRequestDTO requestDTO){
-        Optional<User> findedUser = this.userService.findByEmail(requestDTO.email());
-
-        if(findedUser == null){
-            return ResponseEntity.badRequest().build();
-        }
-
+    public ResponseEntity<?> register(@RequestBody RegisterRequestDTO requestDTO) {
         User user = this.userService.save(requestDTO);
-
         String token = this.tokenService.generateToken(user);
         return ResponseEntity.ok().body(new ResponseDTO(user.getName(), token));
     }
