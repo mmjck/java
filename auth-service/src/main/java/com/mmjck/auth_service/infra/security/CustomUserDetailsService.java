c00d57309f43efeee00636d47538c5a0070f579f
mmjck <msantosjck@gmail.com>
2024-07-23 16:38:12 -0400

    fix: model

--- auth-service/src/main/java/com/mmjck/auth_service/infra/security/CustomUserDetailsService.java
+++ auth-service/src/main/java/com/mmjck/auth_service/infra/security/CustomUserDetailsService.java
@@ -7,22 +7,22 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
-import com.mmjck.auth_service.domain.user.User;
-import com.mmjck.auth_service.repositories.UserRepository;
+import com.mmjck.auth_service.repositories.UserJpaRepository;
+import com.mmjck.auth_service.repositories.jpa.model.UserJpaModel;
 
 @Component
 public class CustomUserDetailsService implements UserDetailsService{
 
-    private UserRepository userRepository;
+    private UserJpaRepository userRepository;
 
-    public CustomUserDetailsService(UserRepository userRepository) {
+    public CustomUserDetailsService(UserJpaRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = this.userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not Found"));
-
+        UserJpaModel user = this.userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not Found"));
+        
 
 
         return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
