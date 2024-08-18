b35765874a8aca8691cd6321881ee78946782f84
mmjck <msantosjck@gmail.com>
2024-08-17 20:43:52 -0400

    project

---
+++ ms/src/main/java/com/bossabox/ms/BoxssaboxApplication.java
@@ -0,0 +1,14 @@
+package com.bossabox.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxssaboxApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BoxssaboxApplication.class, args);
+	}
+
+}
---
+++ ms/src/test/java/com/bossabox/ms/BoxssaboxChallengeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bossabox.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxssaboxChallengeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
