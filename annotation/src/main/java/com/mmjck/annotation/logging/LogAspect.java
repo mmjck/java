194fdc294ca3017894e077ddeda2a183e34d733a
mmjck <msantosjck@gmail.com>
2024-09-08 18:34:08 -0400

    log aspect

--- annotation/src/main/java/com/mmjck/annotation/logging/LogAspect.java
+++ annotation/src/main/java/com/mmjck/annotation/logging/LogAspect.java
@@ -1,2 +1,22 @@
-package com.mmjck.annotation.logging;public class LogAspect {
+package com.mmjck.annotation.logging;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LogAspect {
+
+    @Around("@annotation(Log)")
+    public Object automaticLog(ProceedingJoinPoint jp) throws Throwable {
+        System.out.println("init - " + jp.getSignature());
+        var result =  jp.proceed();
+        System.out.println("end - " + jp.getSignature());
+
+
+        return result;
+
+    }
 }
